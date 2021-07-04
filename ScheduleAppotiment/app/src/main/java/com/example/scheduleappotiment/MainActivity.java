a31020b838c286099ca431ea268f9ccd97739072
siddharth9365 <30820602+siddharth9365@users.noreply.github.com>
2021-07-04 15:57:33 -0400

    Initial Commit

---
+++ ScheduleAppotiment/app/src/main/java/com/example/scheduleappotiment/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.scheduleappotiment;
+
+import android.os.Bundle;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.scheduleappotiment.databinding.ActivityMainBinding;
+import com.example.scheduleappotiment.utility.BaseActivity;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
